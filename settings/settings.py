6601070d3a5d9dd5e2344e4305c35a2a10218d32
abaddonpuff <abaddonpuff@gmail.com>
2024-07-04 20:32:04 -0400

    Character movement enabled, use keyboard arrows

--- settings/settings.py
+++ settings/settings.py
@@ -1,14 +1,12 @@
-import pygame
-
 class Settings:
-    '''
+    """
     A class to store all settings for game
-    '''
+    """
 
     def __init__(self):
-        '''
+        """
         Initialize game's settings
-        '''
+        """
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (130, 50, 50)
