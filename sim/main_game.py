550c51a545ecad01fd475252bc7d605cbe5f166c
Bob Belderbos <bobbelderbos@gmail.com>
2024-07-08 14:59:03 +0200

    check for collisions

--- sim/main_game.py
+++ sim/main_game.py
@@ -7,47 +7,30 @@ from characters.npc.npc import Npc
 
 INPUTBOX_DISPLAY = False
 
-
 class projectG:
-    """
-    Class to manage game assets and behavior
-    """
-
     def __init__(self):
-        """
-        Initialize game and create game resources
-        """
         pygame.init()
-
         self.settings = GameSettings()
         self.input_box_settings = InputBox()
-
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
         pygame.display.set_caption("ProjectG")
         self.player = Player(self)
-        self.type_box = InputBox(self)
+        self.type_box = InputBox()
         self.npc = Npc(self)
+        self.collided = False
 
     def run_game(self):
-        """
-        Start the main loop of the game
-        """
         while True:
-            # Watch for keyboard and mouse events.
             self._check_events()
-            # Check Player movements
             self.player.update()
+            self._check_collisions()
             if self.type_box.active:
                 self._render_typebox()
-            # Make screen visible
             self._update_screen()
 
     def _check_events(self):
-        """
-        Responds to keypresses and mouse events
-        """
         key_map = {
             pygame.K_RIGHT: "moving_right",
             pygame.K_LEFT: "moving_left",
@@ -65,28 +48,26 @@ class projectG:
             elif event.type == pygame.K_SPACE:
                 self.type_box.active = True
 
+    def _check_collisions(self):
+        if self.player.rect.colliderect(self.npc.rect):
+            self.collided = True
+        else:
+            self.collided = False
+
     def _update_screen(self):
-        """
-        Update images on the screen and go to the next screen
-        """
         self.screen.fill(self.settings.bg_color)
         self.player.render()
         self.npc.render()
-
         pygame.display.flip()
+        print(f"collided: {self.collided}")
 
     def _render_typebox(self):
-        """
-        Create an input box when space bar is clicked
-        """
-
         self.font = pygame.font.Font(None, 32)
         self.input_box = pygame.Rect(100, 100, 140, 32)
         self.text = ""
-
         pygame.display.flip()
-
 
 if __name__ == "__main__":
     pG = projectG()
     pG.run_game()
+
